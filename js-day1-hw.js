d09c3549e4084781367e18649c52159b5836f8af
FreshleBlanc <dchang4309@gmail.com>
2023-01-05 18:14:21 -0600

    code wars problems

---
+++ js-day1-hw.js
@@ -0,0 +1,14 @@
+// Code wars Problem 1
+
+function multiply(a, b){
+    return a * b
+   }
+   
+
+// Code wars Problem 2
+
+function evenOrOdd(number) {
+    if(number % 2 === 0) return "Even";
+    return "Odd";
+    
+    }
